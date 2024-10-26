b84091d7e37609ec9c89be74fa03f0b64fc31eaa
betabi-ai <betabi.zhang@gmail.com>
2024-10-26 20:58:34 +0900

    cors

--- graceful_backend/settings.py
+++ graceful_backend/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",  # 添加 CORS 中间件
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
