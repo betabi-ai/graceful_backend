248581b2cba2d30e54287be3abba134220b03ae6
betabi-ai <betabi.zhang@gmail.com>
2024-11-01 11:21:13 +0900

    changed.

--- cpc/tasks.py
+++ cpc/tasks.py
@@ -317,46 +317,20 @@ def campaigns_budget_spider_task(**spider_args):
 
 
 @shared_task()
-def test_spider_task(**spider_args):
+def campaigns_budget_calc_tmp_task(**spider_args):
     """
-    执行 test_spider 爬虫任务
-
+    执行 campaigns_budget_calc_tmp 爬虫任务
+    下载关键词类别报告(按日，按月)
     """
     """
-    curl http://localhost:6800/schedule.json -d arg1=val1 -d project=gracefulRakutenSpiders -d spider=test_spider
+    curl http://localhost:6800/schedule.json -d arg1=val1 -d project=gracefulRakutenSpiders -d spider=campaigns_budget_calc_tmp
     """
-
-    # url = f"{settings.SCRAPYD_URL}/schedule.json"
+    print("Task started")
 
     data = {
         "project": "gracefulRakutenSpiders",
-        "spider": "test_spider",
+        "spider": "campaigns_budget_calc_tmp",
     }
     data.update(spider_args)
     response = requests.post(SCHEDULE_URL, data=data)
     return response.json()
-
-
-@shared_task()
-def campaigns_budget_spider_task_tmp(**spider_args):
-    """
-    执行 test_campaigns_budget_spider 爬虫任务
-
-    """
-    """
-    curl http://localhost:6800/schedule.json -d arg1=val1 -d project=gracefulRakutenSpiders -d spider=test_campaigns_budget_spider
-    """
-
-    # url = f"{settings.SCRAPYD_URL}/schedule.json"
-
-    data = {
-        "project": "gracefulRakutenSpiders",
-        "spider": "test_campaigns_budget_spider",
-    }
-    # print("=========spider_args:", spider_args)
-    data.update(spider_args)
-    try:
-        response = requests.post(SCHEDULE_URL, data=data)
-        return response.json()
-    except Exception as e:
-        return {"error": str(e)}
