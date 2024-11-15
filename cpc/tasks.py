c96fe52e4b4ec32c4766e903e5c8c36b3febc46b
betabi-ai <betabi.zhang@gmail.com>
2024-11-15 20:15:30 +0900

    changed.

--- cpc/tasks.py
+++ cpc/tasks.py
@@ -374,3 +374,23 @@ def campaigns_budget_login_spider_task(**spider_args):
     data.update(spider_args)
     response = requests.post(SCHEDULE_URL, data=data)
     return response.json()
+
+
+@shared_task()
+def ranking_rakuten_task(**spider_args):
+    """
+    执行 ranking_rakuten 爬虫任务
+    刷新rrp的cookies
+    """
+    """
+    curl http://localhost:6800/schedule.json -d arg1=val1 -d project=gracefulRakutenSpiders -d spider=ranking_rakuten
+    """
+    print("Task started")
+
+    data = {
+        "project": "gracefulRakutenSpiders",
+        "spider": "ranking_rakuten",
+    }
+    data.update(spider_args)
+    response = requests.post(SCHEDULE_URL, data=data)
+    return response.json()
