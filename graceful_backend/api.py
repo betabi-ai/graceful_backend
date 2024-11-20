34aac57a59e696494b0a491108d1213a04abde72
betabi-ai <betabi.zhang@gmail.com>
2024-11-20 09:42:01 +0900

    合并api

--- graceful_backend/api.py
+++ graceful_backend/api.py
@@ -9,7 +9,8 @@ from ninja_jwt.controller import NinjaJWTDefaultController
 
 from cpc.api import router as cpc_router
 from reports.api import router as reports_router
-from shares.api import router as shares_router
+
+# from shares.api import router as shares_router
 
 api = NinjaExtraAPI()
 api.register_controllers(NinjaJWTDefaultController)
@@ -21,5 +22,5 @@ def hello(request):
 
 
 api.add_router("/cpc", cpc_router)
-api.add_router("/share", shares_router)
+# api.add_router("/share", shares_router)
 api.add_router("/reports", reports_router)
